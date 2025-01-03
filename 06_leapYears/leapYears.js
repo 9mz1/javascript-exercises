00b25c58b5b3d154841df3a606a2c3454b4f409e
9mz1 <muzyyyyyyy@gmail.com>
2025-01-03 14:27:47 +0400

    Completed fifth and sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // const century = year % 100 == 0;
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+console.log(leapYears(1600));
 // Do not edit below this line
 module.exports = leapYears;
